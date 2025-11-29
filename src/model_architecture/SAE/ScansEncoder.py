2d547aa64631671fae5db408d8a1d426ce71ae61
mikic202 <mikolaj.chomanski@gmail.com>
2025-11-29 15:18:36 +0100

    fix: update training loop for basic autoencoder training

--- src/model_architecture/SAE/ScansEncoder.py
+++ src/model_architecture/SAE/ScansEncoder.py
@@ -5,18 +5,17 @@ class ScansEncoder3d(nn.Module):
     def __init__(self, input_channels=1, feature_dim=256):
         super(ScansEncoder3d, self).__init__()
         self.encoder = nn.Sequential(
-            nn.Conv3d(input_channels, 32, kernel_size=3, stride=2, padding=1),
-            nn.BatchNorm3d(32),
-            nn.ReLU(),
-            nn.Conv3d(32, 64, kernel_size=3, stride=2, padding=1),
+            nn.Conv3d(input_channels, 64, kernel_size=3, stride=2, padding=1),
+            nn.BatchNorm3d(64),
+            nn.LeakyReLU(),
+            nn.Conv3d(64, 64, kernel_size=3, stride=2, padding=1),
             nn.BatchNorm3d(64),
             nn.ReLU(),
             nn.Conv3d(64, 128, kernel_size=3, stride=2, padding=1),
             nn.BatchNorm3d(128),
             nn.ReLU(),
-            nn.AdaptiveAvgPool3d((1, 1, 1)),
         )
-        self.fc = nn.Linear(128, feature_dim)
+        self.fc = nn.Linear(128 * 8 * 8 * 8, feature_dim)
 
     def forward(self, x):
         x = self.encoder(x)
@@ -28,21 +27,21 @@ class ScansEncoder3d(nn.Module):
 class ScansDecoder3d(nn.Module):
     def __init__(self, output_channels=1, feature_dim=256):
         super(ScansDecoder3d, self).__init__()
-        self.fc = nn.Linear(feature_dim, 128 * 4 * 4 * 4)
+        self.fc = nn.Linear(feature_dim, 128 * 8 * 8 * 8)
         self.decoder = nn.Sequential(
-            nn.ConvTranspose3d(128, 64, kernel_size=4, stride=2, padding=1),
+            nn.ConvTranspose3d(128, 64, kernel_size=3, stride=2, padding=0),
             nn.BatchNorm3d(64),
+            nn.LeakyReLU(),
+            nn.ConvTranspose3d(64, 64, kernel_size=3, stride=2, padding=1),
+            nn.BatchNorm3d(64),
+            nn.LeakyReLU(),
+            nn.ConvTranspose3d(64, output_channels, kernel_size=3, stride=2, padding=1),
             nn.ReLU(),
-            nn.ConvTranspose3d(64, 32, kernel_size=4, stride=2, padding=1),
-            nn.BatchNorm3d(32),
-            nn.ReLU(),
-            nn.ConvTranspose3d(32, output_channels, kernel_size=4, stride=2, padding=1),
-            nn.Sigmoid(),
         )
 
     def forward(self, x):
         x = self.fc(x)
-        x = x.view(x.size(0), 128, 4, 4, 4)
+        x = x.view(x.size(0), 128, 8, 8, 8)
         x = self.decoder(x)
         return x
 
@@ -56,4 +55,7 @@ class ScansAutoencoder3d(nn.Module):
     def forward(self, x):
         encoded = self.encoder(x)
         decoded = self.decoder(encoded)
+        decoded = nn.functional.interpolate(
+            decoded, size=x.shape[2:], mode="trilinear", align_corners=False
+        )
         return decoded
--- src/model_training/ScansEncoderTraining.py
+++ src/model_training/ScansEncoderTraining.py
@@ -4,23 +4,26 @@ from torch import optim
 import torchio as tio
 import torch
 import time
+import os
+from src.model_training.training_helpers.loggers import WandbLogger
 
-ENCODED_DIM = 256
-NUM_EPOCHS = 100
-BATCH_SIZE = 16
+ENCODED_DIM = 1024
+NUM_EPOCHS = 20
+BATCH_SIZE = 2
+LR = 0.0008
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model = ScansAutoencoder3d(
     input_channels=1, output_channels=1, feature_dim=ENCODED_DIM
 ).to(device)
 
-optimizer = optim.Adam(model.parameters(), lr=0.001)
+optimizer = optim.Adam(model.parameters(), lr=LR)
 scheduler = optim.lr_scheduler.ExponentialLR(optimizer=optimizer, gamma=0.94)
 
 
 dataset_transform = tio.transforms.Compose(
     [
-        tio.transforms.Resize((128, 128, 128)),
+        tio.transforms.Resize((64, 64, 64)),
     ]
 )
 
@@ -33,24 +36,40 @@ train_dataloader = torch.utils.data.DataLoader(
     dataset, batch_size=BATCH_SIZE, shuffle=True
 )
 
+data_logger = WandbLogger(
+    "knee-scanner",
+    config={
+        "model": "image-to-latent-autoencoder",
+        "encoded_dim": ENCODED_DIM,
+        "lr": LR,
+        "batch_size": BATCH_SIZE,
+        "loss_function": "MSE",
+    },
+)
 
-for epoch in range(NUM_EPOCHS):
-    model.train()
-    epoch_loss = 0.0
-    for batch in train_dataloader:
-        batch = batch.to(device)
-        optimizer.zero_grad()
-        outputs = model(batch.float())
-        loss = torch.nn.functional.mse_loss(outputs, batch.float())
-        loss.backward()
-        optimizer.step()
-        epoch_loss += loss.item() * batch.size(0)
+try:
+    if not os.path.exists("models"):
+        os.makedirs("models")
+    for epoch in range(NUM_EPOCHS):
+        model.train()
+        epoch_loss = 0.0
+        for batch in train_dataloader:
+            batch = batch.to(device)
+            optimizer.zero_grad()
+            outputs = model(batch.float())
+            loss = torch.nn.functional.mse_loss(outputs, batch.float())
+            loss.backward()
+            optimizer.step()
+            epoch_loss += loss.item() * batch.size(0)
 
-    epoch_loss /= len(train_dataloader.dataset)
-    print(f"Epoch [{epoch+1}/{NUM_EPOCHS}], Loss: {epoch_loss:.4f}")
+        epoch_loss /= len(train_dataloader.dataset)
+        print(f"Epoch [{epoch+1}/{NUM_EPOCHS}], Loss: {epoch_loss:.4f}")
+        data_logger.log({"loss": epoch_loss})
+        if (epoch + 1) % 5 == 0:
+            scheduler.step()
 
-    scheduler.step()
-
+except KeyboardInterrupt:
+    print("Training interrupted. Saving the model...")
 
 model_scripted = torch.jit.script(model.cpu())
 model_scripted.save(f"models/autoencoder_model_{time.time()}.pt")
