a4ea24bb886216b5db6ce751234f3cd7612eadc8
Dominika <01168920@pw.edu.pl>
2026-01-14 14:30:23 +0100

    feat: Update module code

--- src/web_app/main.py
+++ src/web_app/main.py
@@ -17,9 +17,8 @@ from src.explainibility.basic_gradient_based_methods import (
 
 from src.web_app.config import AppConfig
 
-PROJECT_ROOT = Path(__file__).resolve().parents[2]
-if str(PROJECT_ROOT) not in sys.path:
-    sys.path.insert(0, str(PROJECT_ROOT))
+if str(AppConfig.PROJECT_ROOT) not in sys.path:
+    sys.path.insert(0, str(AppConfig.PROJECT_ROOT))
 
 
 logging.basicConfig(
